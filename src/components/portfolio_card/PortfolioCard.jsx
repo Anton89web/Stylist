7928795e2d7921b0f4fd721a95f29765b76a5668
Anton <fenix-live@mail.ru>
2022-11-17 16:40:51 +0300

    change bg

--- src/components/portfolio_card/PortfolioCard.jsx
+++ src/components/portfolio_card/PortfolioCard.jsx
@@ -4,8 +4,6 @@ import "./portfolioCard.sass"
 const PortfolioCard = ({title, subtitle, descr, foto}) => {
  return(
   <div className="portfolio__card">
-    <h3 className="portfolio__card_title">{title}</h3>
-    {subtitle ? <h4 className="portfolio__card_subtitle">{subtitle}</h4> : ""}
     <p className="portfolio__card_descr">{descr}</p>
     <div className="portfolio__card_grid session">
       {foto.map((e, i) => <img className="portfolio__img" src={e} alt="" key={i}/>)}
